b086c40ae9b8ef35a9a7d57301b73d5ddcf2ded8
sahatilfat <sahatilfat@gmail.com>
2021-05-23 02:37:05 +0700

    menambahkan kode res

---
+++ res.js
@@ -0,0 +1,11 @@
+"use strict"; //artinya kode ini akan dijalankan dg ketat
+
+exports.ok = function (values, res) {
+  var data = {
+    status: 200,
+    values: values,
+  };
+
+  res.json(data);
+  res.end();
+};
