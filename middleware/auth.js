42a71d8172583198e0ad33457c53d4f853537a94
sahatilfat <sahatilfat@gmail.com>
2021-05-29 11:24:01 +0700

    oke role beres

--- middleware/auth.js
+++ middleware/auth.js
@@ -91,3 +91,7 @@ exports.login = function (req, res) {
     }
   });
 };
+
+exports.halamanrahasia = function (req, res) {
+  response.ok("Halaman ini hanya untuk user dengan role = 2", res);
+};
--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var auth = require("./auth");
+var verifikasi = require("./verifikasi");
 var router = express.Router();
 
 // daftarkan menu registrasi
 router.post("/api/v1/register", auth.registrasi);
 router.post("/api/v1/login", auth.login);
+
+// alamat yg perlu otorisasi
+router.get("/api/v1/rahasia", verifikasi(), auth.halamanrahasia);
 
 module.exports = router;
