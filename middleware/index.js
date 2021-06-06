e3402c414460c1979864328fe7fd378cb17c253c
sahatilfat <sahatilfat@gmail.com>
2021-06-06 08:51:20 +0700

    perbaikan utk halaman yg membutuhkan verifikasi

--- middleware/index.js
+++ middleware/index.js
@@ -9,6 +9,6 @@ router.post("/api/v1/login", auth.login);
 
 // alamat yg perlu otorisasi
 // router.get("/api/v1/rahasia", verifikasi(), auth.halamanrahasia);
-router.get("/api/vi/admin/mahasiswa", verifikasi(1), auth.adminmahasiswa);
+router.get("/api/v1/admin/mahasiswa", verifikasi(1), auth.adminmahasiswa);
 
 module.exports = router;
--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -1,34 +1,67 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/secret");
 
-function verifikasi() {
+// function verifikasi() {
+//   return function (req, rest, next) {
+//     var role = req.body.role;
+//     // cek authorizzation header
+//     var tokenWithBearer = req.headers.authorization;
+//     if (tokenWithBearer) {
+//       var token = tokenWithBearer.split(" ")[1];
+//       // verifikasi
+//       jwt.verify(token, config.secret, function (err, decoded) {
+//         if (err) {
+//           return rest
+//             .status(401)
+//             .send({ auth: false, message: "token tidak terdaftar" });
+//         } else {
+//           if (role == 2) {
+//             req.auth = decoded;
+//             next();
+//           } else {
+//             return rest
+//               .status(401)
+//               .send({ auth: false, message: "gagal mengotorisasi role anda" });
+//           }
+//         }
+//       });
+//     } else {
+//       return rest
+//         .status(401)
+//         .send({ auth: false, message: "Token tidak tersedia" });
+//     }
+//   };
+// }
+
+function verifikasi(role) {
   return function (req, rest, next) {
-    var role = req.body.role;
-    // cek authorizzation header
+    //cek authorizzation header
     var tokenWithBearer = req.headers.authorization;
+
     if (tokenWithBearer) {
       var token = tokenWithBearer.split(" ")[1];
-      // verifikasi
+
+      //verifikasi
       jwt.verify(token, config.secret, function (err, decoded) {
         if (err) {
           return rest
             .status(401)
-            .send({ auth: false, message: "token tidak terdaftar" });
+            .send({ auth: false, message: "Token tidak terdaftar!" });
         } else {
-          if (role == 2) {
+          if (role == 1) {
             req.auth = decoded;
             next();
           } else {
             return rest
               .status(401)
-              .send({ auth: false, message: "gagal mengotorisasi role anda" });
+              .send({ auth: false, message: "Gagal mengotorisasi role anda!" });
           }
         }
       });
     } else {
       return rest
         .status(401)
-        .send({ auth: false, message: "Token tidak tersedia" });
+        .send({ auth: false, message: "Token tidak tersedia!" });
     }
   };
 }
